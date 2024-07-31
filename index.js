187054bb8b9250c691807feeca22f3c2730a2b4f
teshager <teshager8922@gmail.com>
2024-07-31 18:35:26 +0300

    grab the giphy translate api functionality

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const fetchWeatherData=async(city)=>{
- const rawData= await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=748MPM7WG568SVR2CHFVUGF23&contentType=json`);
- const parsedData= await rawData.json();
+    const baseUrl='https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+    const API_KEY="748MPM7WG568SVR2CHFVUGF23";
+    const rawData= await fetch(`${baseUrl}${city}?key=${API_KEY}&contentType=json`);
+    const parsedData= await rawData.json();
  return parsedData;
 }
 
@@ -11,3 +13,19 @@ fetchWeatherData('bahirdar').then((data)=>{
     console.log(data.days[0].hours[0].icon);
     console.log((data['days'].length));
 });
+
+const translateWeatherToGif=async (weather)=>{
+ const baseUrl="https://api.giphy.com/v1/gifs/translate";
+ const API_KEY="E03DHQa08uJqlEZKuJh8Jzlpnyq3D52f";
+const url= `${baseUrl}?api_key=${API_KEY}&s=${weather}`;
+
+ const rawData=await fetch(url);
+ const parsedData= await rawData.json();
+ return parsedData;
+
+}
+
+translateWeatherToGif('rain').then(urls=>{
+
+    console.log(urls.data.url)});
+
