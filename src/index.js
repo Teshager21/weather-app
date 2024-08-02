d00f06d51051602e17b08fcdfcb507709ef30f7b
teshager <teshager8922@gmail.com>
2024-08-02 13:23:38 +0300

    add an input to collect user input for location

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@
 import './style.css'
 import loading from './assets/loading.gif'
 
+let location='';
+
 const loadingGif = new Image();
 loadingGif.src=loading;
 loadingGif.classList.add('loading');
-document.querySelector('.weather-gif').append(loadingGif);
+
 
 
 const fetchWeatherData=async(city)=>{
+    document.querySelector('.weather-gif').append(loadingGif);
     const baseUrl='https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
     const API_KEY="748MPM7WG568SVR2CHFVUGF23";
     const rawData= await fetch(`${baseUrl}${city}?key=${API_KEY}&contentType=json`);
@@ -16,16 +19,7 @@ const fetchWeatherData=async(city)=>{
  return parsedData;
 }
 
-fetchWeatherData('bahirdar').then((data)=>{
-    console.log(data.days[0].hours[0].conditions);
-    const iconUrl=data.days[0].hours[0].icon;
-    return iconUrl;
-}).then ((url)=>translateWeatherToGif(url)).then(urls=>{
-    const weatherImg= document.createElement('iframe')
-    weatherImg.src=urls.data.embed_url;
-    document.querySelector('.weather-gif').innerHTML='';
-    document.querySelector('.weather-gif').append(weatherImg);
-})
+
 
 const translateWeatherToGif=async (weather)=>{
  const baseUrl="https://api.giphy.com/v1/gifs/translate";
@@ -37,6 +31,23 @@ const url= `${baseUrl}?api_key=${API_KEY}&s=${weather}`;
  return parsedData;
 
 }
+
+
+document.getElementById('searchBtn').addEventListener('click',()=>{
+    location=document.getElementById('locationInput').value;
+    fetchWeatherData(location).then((data)=>{
+        console.log(data.days[0].hours[0].conditions);
+        const iconUrl=data.days[0].hours[0].icon;
+        return iconUrl;
+    }).then ((url)=>translateWeatherToGif(url)).then(urls=>{
+        const weatherImg= document.createElement('iframe')
+        weatherImg.src=urls.data.embed_url;
+        weatherImg.setAttribute('alt',urls.data.embed_url);
+        const weatherGif= document.querySelector('.weather-gif');
+        weatherGif.innerHTML='';
+        weatherGif.append(weatherImg);
+    })
+})
 
 
 
