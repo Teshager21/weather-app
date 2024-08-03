c115456db3f14401a69fe910b97d75260e57369b
teshager <teshager8922@gmail.com>
2024-08-03 13:50:34 +0300

    show the current location weather when the site loads

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const suggestionsList=document.querySelector('.suggestions');
 const resolvedAddress= document.getElementById('resolvedAddress');
 const details=document.createElement('ul');
 details.classList.add('details');
+const API_KEY="E03DHQa08uJqlEZKuJh8Jzlpnyq3D52f";
 let weatherCondition='';
 let weatherDescription='';
 let allLocationNames=[];
@@ -19,7 +20,8 @@ suggestionsList.addEventListener('click',(e)=>{
     if(e.target.nodeName==='LI') {
         locationInput.value=e.target.textContent;
         suggestionsList.innerHTML='';
-        serveWeatherData();
+        serveWeatherData(locationInput.value);
+        location=locationInput.value;
     }
 })
 
@@ -37,7 +39,7 @@ const fetchWeatherData=async(city)=>{
 
 const translateWeatherToGif=async (weather)=>{
  const baseUrl="https://api.giphy.com/v1/gifs/translate";
- const API_KEY="E03DHQa08uJqlEZKuJh8Jzlpnyq3D52f";
+
 const url= `${baseUrl}?api_key=${API_KEY}&s=${weather}weather`;
 
  const rawData=await fetch(url);
@@ -47,18 +49,19 @@ const url= `${baseUrl}?api_key=${API_KEY}&s=${weather}weather`;
 }
 
 
-document.getElementById('searchBtn').addEventListener('click',()=>serveWeatherData());
+document.getElementById('searchBtn').addEventListener('click',()=>serveWeatherData(locationInput.value));
 
-const serveWeatherData=()=>{
-    location=locationInput.value;
+const serveWeatherData=(location)=>{
+    // location=locationInput.value;
+    console.log('location is',location);
     fetchWeatherData(location)
         .then((data)=>grabUrlFromWeatherData(data))
         .then ((url)=>translateWeatherToGif(url))
-        .then((urls)=>displayWeatherData(urls));
+        .then((urls)=>displayWeatherData(urls))
+        .catch(error=>console.error('HERE GOES',error));
 }
 
 const grabUrlFromWeatherData=(data)=>{
-    console.log(data);
     details.innerHTML='';
     const temprature= document.createElement('li');
     const humidity= document.createElement('li');
@@ -69,7 +72,6 @@ const grabUrlFromWeatherData=(data)=>{
     resolvedAddress.textContent=data.resolvedAddress;
     weatherCondition=data.currentConditions.conditions;
     weatherDescription=data.description;
-    // weatherCondition=data.days[0].hours[0].conditions;
     const iconUrl=data.days[0].hours[0].icon;
     details.append(temprature,humidity,precipitation);
     return iconUrl;   
@@ -85,13 +87,12 @@ const displayWeatherData=(urls)=>{
     const conditionHeading= document.createElement('h2');
     const description=document.createElement('small');
     description.textContent=weatherDescription;
-    conditionHeading.textContent=`The condition in ${location} is ${weatherCondition}`;
+    conditionHeading.textContent=`The weather in ${location} is ${weatherCondition}`;
     weatherGif.append(conditionHeading,description,details);  
 }
 const fetchAllLocationNames=async()=>{
  const locations= await fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
  const locationsParsed= await locations.json();
-//  console.log(locationsParsed);
  const locationNames=[];
  for(let location of locationsParsed){
     locationNames.push(location.city);
@@ -124,7 +125,31 @@ const regex=new RegExp(`^${subString}`,'gi');
 const filteredLocationNames=locations.filter(location=>regex.test(location));
 return filteredLocationNames;
 }
+/**************************************************************************************
+ *                                     GEO-LOCATION
+ * *************************************************************************************/
 
+const getCurrentLocation=()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+        getCityName(position);
+    });     
+}
+// let currentCity='';
+const getCityName=async (position)=>{
+const longitude= position.coords.longitude;
+const latitude= position.coords.latitude;
+const url =`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+ const rawData= await fetch(url);
+ const parsedData= await rawData.json();
+ location=parsedData.city;
+ serveWeatherData(location);
+ console.log('city name',location);
+}
+
+
+// getCityName(37.42159,-122.0837);
+getCurrentLocation();
+// serveWeatherData(currentCity);
 
 
 
