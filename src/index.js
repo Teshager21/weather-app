0419c85a2ef1aedb2b35213adf6b9fbb2855d268
teshager <teshager8922@gmail.com>
2024-08-03 05:49:57 +0300

    add weather details on display

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import loading from './assets/loading.gif'
 let location='';                    
 const locationInput= document.getElementById('locationInput');
 const suggestionsList=document.querySelector('.suggestions');
+const resolvedAddress= document.getElementById('resolvedAddress');
+const details=document.createElement('ul');
+details.classList.add('details');
 let weatherCondition='';
+let weatherDescription='';
 let allLocationNames=[];
 const loadingGif = new Image();
 loadingGif.src=loading;
@@ -34,7 +38,7 @@ const fetchWeatherData=async(city)=>{
 const translateWeatherToGif=async (weather)=>{
  const baseUrl="https://api.giphy.com/v1/gifs/translate";
  const API_KEY="E03DHQa08uJqlEZKuJh8Jzlpnyq3D52f";
-const url= `${baseUrl}?api_key=${API_KEY}&s=${weather} weather`;
+const url= `${baseUrl}?api_key=${API_KEY}&s=${weather}weather`;
 
  const rawData=await fetch(url);
  const parsedData= await rawData.json();
@@ -54,21 +58,35 @@ const serveWeatherData=()=>{
 }
 
 const grabUrlFromWeatherData=(data)=>{
-    weatherCondition=data.days[0].hours[0].conditions;
+    console.log(data);
+    details.innerHTML='';
+    const temprature= document.createElement('li');
+    const humidity= document.createElement('li');
+    const precipitation= document.createElement('li');
+    temprature.textContent='Temprature: ' + data.currentConditions.temp;
+    humidity.textContent='Humidity: ' + data.currentConditions.humidity;
+    precipitation.textContent='Precipitation: ' + data.currentConditions.precip;
+    resolvedAddress.textContent=data.resolvedAddress;
+    weatherCondition=data.currentConditions.conditions;
+    weatherDescription=data.description;
+    // weatherCondition=data.days[0].hours[0].conditions;
     const iconUrl=data.days[0].hours[0].icon;
+    details.append(temprature,humidity,precipitation);
     return iconUrl;   
 }
 
 const displayWeatherData=(urls)=>{
-    const weatherImg= document.createElement('iframe')
+    const weatherImg= document.createElement('iframe');
     weatherImg.src=urls.data.embed_url;
     weatherImg.setAttribute('alt',urls.data.embed_url);
     const weatherGif= document.querySelector('.weather-gif');
     weatherGif.innerHTML='';
     weatherGif.append(weatherImg);
     const conditionHeading= document.createElement('h2');
+    const description=document.createElement('small');
+    description.textContent=weatherDescription;
     conditionHeading.textContent=`The condition in ${location} is ${weatherCondition}`;
-    weatherGif.append(conditionHeading);  
+    weatherGif.append(conditionHeading,description,details);  
 }
 const fetchAllLocationNames=async()=>{
  const locations= await fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
